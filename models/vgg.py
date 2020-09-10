49c7c4ca8a1943cc5a5a39a71170ddad3d23be7f
rishabh-16 <akchitra99@gmail.com>
2020-09-10 13:09:53 +0000

    updated vgg model

--- models/vgg.py
+++ models/vgg.py
@@ -10,16 +10,8 @@ class VGG(BaseModel):
     def __init__(self, features, num_classes=1000):
         super(VGG, self).__init__()
         self.features = features
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
+        self.avgpool = nn.AdaptiveAvgPool2d()
+        self.classifier = nn.Linear(512, num_classes)
         self.init_weights()
 
     def forward(self, x):
