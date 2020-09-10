fdb10d206f1dd9841b346e8359c1de50cc14a69f
Rishabh Tiwari <akchitra99@gmail.com>
2020-09-11 00:49:52 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,5 +7,5 @@ echo enter budget
 read budget
 
 python pretraining.py $dataset $model --epochs 300
-python pruning.py dataset model --budget $budget
+python pruning.py $dataset $model --budget $budget
 python finetuning --name $model\_$dataset\_$budget --epochs 300 
