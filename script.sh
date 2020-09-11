90e804ebf0b83a46f9551b75bc9aded136eb77ab
Rishabh Tiwari <akchitra99@gmail.com>
2020-09-11 09:46:49 +0000

    minor fix

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+echo 
+python pretraining.py $1 $2 --epochs 300
+python pruning.py $1 $2 --Vc 0.0625
+python finetuning.py --name $2\_$1\_0.0625 --epochs 300
 
-python pretraining.py $0 $1 --epochs 300
-python pruning.py $0 $1 --Vc 0.0625
-python finetuning --name $1\_$0\_0.0625 --epochs 300
+python pruning.py $1 $2 --Vc 0.125
+python finetuning.py --name $2\_$1\_0.125 --epochs 300
 
-python pruning.py $0 $1 --Vc 0.125
-python finetuning --name $1\_$0\_0.125 --epochs 300
+python pruning.py $1 $2 --Vc 0.25
+python finetuning.py --name $2\_$1\_0.25 --epochs 300
 
-python pruning.py $0 $1 --Vc 0.25
-python finetuning --name $1\_$0\_0.25 --epochs 300
-
-python pruning.py $0 $1 --Vc 0.5
-python finetuning --name $1\_$0\_0.5 --epochs 300
+python pruning.py $1 $2 --Vc 0.5
+python finetuning.py --name $2\_$1\_0.5 --epochs 300
