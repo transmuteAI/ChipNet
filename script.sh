479bebd35eaa5512bb8e600df8edcc77da37872f
Udbhav Bamba <ubamba98@gmail.com>
2020-09-12 11:46:24 +0530

    add correct vc to fine tuning

--- script.sh
+++ script.sh
@@ -2,13 +2,13 @@
 echo 
 python pretraining.py $1 $2 --epochs 300
 python pruning.py $1 $2 --Vc 0.0625
-python finetuning.py $1 $2 --name $2\_$1\_0.0625 --epochs 300
+python finetuning.py $1 $2 --name $2\_$1\_0.0625 --epochs 300 --Vc 0.0625
 
 python pruning.py $1 $2 --Vc 0.125
-python finetuning.py $1 $2 --name $2\_$1\_0.125 --epochs 300
+python finetuning.py $1 $2 --name $2\_$1\_0.125 --epochs 300 --Vc 0.125
 
 python pruning.py $1 $2 --Vc 0.25
-python finetuning.py $1 $2 --name $2\_$1\_0.25 --epochs 300
+python finetuning.py $1 $2 --name $2\_$1\_0.25 --epochs 300 --Vc 0.25
 
 python pruning.py $1 $2 --Vc 0.5
-python finetuning.py $1 $2 --name $2\_$1\_0.5 --epochs 300
+python finetuning.py $1 $2 --name $2\_$1\_0.5 --epochs 300 --Vc 0.5
