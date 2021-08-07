4f44da9f6d4864cd756c06ed4e977bbcb71c9b19
gktejus <gktejus@gmail.com>
2021-08-07 18:50:38 +0530

    added label smoothing

--- utils.py
+++ utils.py
@@ -14,6 +14,22 @@ def seed_everything(seed):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
 
+
+class CrossEntropyLabelSmooth(nn.Module):
+
+  def __init__(self, num_classes, epsilon):
+    super(CrossEntropyLabelSmooth, self).__init__()
+    self.num_classes = num_classes
+    self.epsilon = epsilon
+    self.logsoftmax = nn.LogSoftmax(dim=1)
+
+  def forward(self, inputs, targets):
+    log_probs = self.logsoftmax(inputs)
+    targets = torch.zeros_like(log_probs).scatter_(1, targets.unsqueeze(1), 1)
+    targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
+    loss = (-targets * log_probs).mean(0).sum()
+    return loss
+
 def get_mask_dict(own_state, state_dict):
     for name, param in state_dict.items():
         if name not in own_state:
